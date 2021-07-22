08d66792b1f1d0693bb7ffe783dbe450c333d461
manoj <manojsambare1497@gmail.com>
2021-07-22 13:33:52 +0530

    java project added

---
+++ JavaProject/Day38-GitFirstJava/src/com/manojsambare/java/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.manojsambare.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world- Git");
+
+	}
+
+}
