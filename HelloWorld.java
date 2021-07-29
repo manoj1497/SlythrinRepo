eaff677f1fc5f2151f44186c4af59d587226bad6
nikhildambal <71750572+nikhildambal@users.noreply.github.com>
2021-07-29 11:59:10 +0530

    Update HelloWorld.java
    newchanges

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
-public Class jenkins{
+public class jenkins{
   
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World")
+  public static void main(String[] args) {
+    System.out.println("Hello World");
     }
 }
