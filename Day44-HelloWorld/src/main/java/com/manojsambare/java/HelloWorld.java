4fc0a8f6acea0c031417f8a25b57db3e84e47766
Admin <Admin@DESKTOP-20TECJ8.mshome.net>
2021-07-29 15:20:13 +0530

    add maven project

---
+++ Day44-HelloWorld/src/main/java/com/manojsambare/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.manojsambare.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world- maven");
+
+	}
+
+}
